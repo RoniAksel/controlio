dc8911f94ecde054792c1980ee2e26ee6929c7d1
Roni Akselrod <roniaksel@gmail.com>
2021-04-21 07:31:30 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,2 +1,26 @@
 
+auth.onAuthStateChanged(user=>{
+    if(user){
+        const userName = document.querySelector('#userName');
+        db.collection('users').doc(user.uid).get().then(snapshot=>{
+            userName.innerHTML = `Welcome ${snapshot.data().Name}`
+        })
+    }
+})
+
+
+// auth.onAuthStateChanged(user => {
+//     if (user) {
+//         console.log('user is signed in at users.html');
+//     }
+//     else {
+//         alert('your login session has expired or you have logged out, login again to continue');
+//         location = "index.html";
+//     }
+// })
+
+
+// userName.innerHTML = 
+
+
 
